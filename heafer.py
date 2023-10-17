208a9b95b1664ffe482fa30b5d57faa154d3531e
LEGENDARYACE <132158919+Legendhj@users.noreply.github.com>
2023-10-17 10:53:54 +0600

    Add files via upload

---
+++ heafer.py
@@ -0,0 +1,26 @@
+import os
+
+
+def chk():
+	print('FUCK YOU')
+	print(42*'=')
+	print('RANDOM CLONE')
+	print('EXIT')
+	option = input('CHOICE>>')
+	if option=='1':
+		fuck()
+	
+  
+
+        
+def fuck():
+	print('FUCK YOU BICH')
+	print(42*'=')
+	os.system(' cd /data/data/com.termux/files/usr/bin/')
+	
+	
+
+
+
+    
+chk()
